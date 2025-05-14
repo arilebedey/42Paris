915ebe4d8755ceef7abcf18a966122542522710b
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-05-14 15:56:34 +0200

    [PS] structures def

---
+++ push_swap/push_swap/push_swap.h
@@ -0,0 +1,19 @@
+#ifndef PUSH_SWAP_H
+
+# define PUSH_SWAP_H
+
+typedef struct t_ {
+	int	*stack;
+	int	top;
+	int	bottom;
+	int size;
+} t_stack;
+
+typedef struct s_stack {
+	int	*stack;
+	int	top;
+	int	bottom;
+	int size;
+} t_stack;
+
+#endif
