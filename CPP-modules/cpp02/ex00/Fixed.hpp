c0ce49b87b71d46351f34314cc94f989cf15e780
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-10-24 22:53:53 +0200

    [cpp02] 00

---
+++ CPP-modules/cpp02/ex00/Fixed.hpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+class Fixed {
+public:
+  Fixed();
+  Fixed(const Fixed &other);
+  Fixed &operator=(const Fixed &other);
+  ~Fixed();
+
+  int getRawBits(void) const;
+  void setRawBits(int const raw);
+
+private:
+  int _rawBits;
+  static const int _fractionalBits = 8;
+};
