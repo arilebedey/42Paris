91fda867e2c69ce46b49a9688106534e75847adf
arilebedey <34816154+arilebedey@users.noreply.github.com>
2025-10-25 13:59:17 +0200

    [cpp 03] 00

---
+++ CPP-modules/cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+class ClapTrap {
+private:
+  std::string _name;
+  int _hitPoints;
+  unsigned int _energyPoints;
+  unsigned int _attackDamage;
+
+public:
+  ClapTrap();
+  ClapTrap(const std::string &name);
+  ClapTrap(const ClapTrap &src);
+  ClapTrap operator=(const ClapTrap &src);
+  ~ClapTrap();
+
+  void attack(const std::string &target);
+  void takeDamage(unsigned int amount);
+  void beRepaired(unsigned int amount);
+};
