13cfbc55656d3be035c7619a6ac1f6b39807399a
arilebedey <34816154+arilebedey@users.noreply.github.com>
2026-01-14 10:59:30 +0100

    exam5

---
+++ exam5prep/bigint/bigint.hpp
@@ -0,0 +1,41 @@
+#ifndef BIGINT_HPP
+#define BIGINT_HPP
+
+#include <vector>
+
+class bigint {
+private:
+  std::vector<int> digits;
+
+public:
+  bigint();
+  ~bigint();
+  bigint(unsigned long long);
+  bigint(const bigint &other);
+
+  bigint operator+(const bigint &other) const;
+  bigint &operator=(const bigint &other);
+  bigint &operator+=(const bigint &other);
+
+  bigint &operator++();
+  bigint operator++(int);
+
+  bigint &operator<<=(unsigned int s);
+  bigint &operator>>=(const bigint &s);
+
+  bigint operator<<(unsigned int s) const;
+  bigint operator>>(unsigned int s) const;
+
+  bool operator<(const bigint &other) const;
+  bool operator>(const bigint &other) const;
+
+  bool operator==(const bigint &other) const;
+  bool operator!=(const bigint &other) const;
+
+  bool operator<=(const bigint &other) const;
+  bool operator>=(const bigint &other) const;
+
+  friend std::ostream &operator<<(std::ostream &os, const bigint &bi);
+};
+
+#endif
