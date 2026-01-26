8e6bc0383fd9c63fdb27f0a7cc2ba36b179cb2e1
arilebedey <34816154+arilebedey@users.noreply.github.com>
2026-01-26 11:22:48 +0100

    e5: poly: create constructor and op= for set class which takes the searchable_bag abstract class

--- exam5prep/polyset/polyset/set.cpp
+++ exam5prep/polyset/polyset/set.cpp
@@ -1,2 +1,18 @@
 #include "set.hpp"
+#include "searchable_bag.hpp"
 
+// no default constructor because _bag needs to be initialized
+
+// constructor needs `bag` as reference since abstract class
+// searchable_bag cannot be copied by value or intanciated
+set::set(searchable_bag &bag) : _bag(bag) {};
+
+set::~set() {};
+
+set::set(const set &other) : _bag(other._bag) {};
+
+// references can only be initialized once, hence no reassignment
+set &set::operator=(const set &other) {
+  (void)other;
+  return *this;
+};
--- exam5prep/polyset/polyset/set.hpp
+++ exam5prep/polyset/polyset/set.hpp
@@ -1,4 +1,24 @@
 #ifndef SET_HPP
 #define SET_HPP
 
+#include "searchable_bag.hpp"
+
+class set {
+public:
+  set(searchable_bag &bag);
+  ~set();
+  set(const set &other);
+  set &operator=(const set &other);
+
+  void insert(int);
+  void insert(int *, int);
+  void print() const;
+  void clear();
+
+  bool has(int i) const;
+
+private:
+  searchable_bag &_bag;
+};
+
 #endif
